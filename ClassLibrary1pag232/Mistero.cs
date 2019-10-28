bb476322cc6e6a922dae9b05b7f995cf18d51c1e
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-28 09:51:51 +0100

    Add project files.

---
+++ ClassLibrary1pag232/Mistero.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1pag232
+{
+    public class Mistero
+    {
+        public static int Mistero1(int x, int y)
+        {
+            int prec;
+            if (y == 0)
+                return x;
+            prec = Mistero1(x, y - 1) + 1;
+            return prec;
+        }
+        public static int mistero2(int x,int y)
+        {
+
+            if (y == 0)
+                return 0;
+            return (Mistero1(x, mistero2(x, y - 1)));
+            
+        }
+        public static int mistero3(int x,int y)
+        {
+            if (y == 0)
+                return 1;
+            return mistero2(x,mistero3(x,y-1)));
+        }
+        public static int Mistero4(long n)
+        {
+           
+            if 
+
+        }
+            
+    }
+}
+
+   
+
---
+++ Ese_1pag232/Program.cs
@@ -0,0 +1,28 @@
+﻿using ClassLibrary1pag232;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ese_1pag232
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserici il valore della x");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("inserici il valore della y");
+            int y = int.Parse(Console.ReadLine());
+            int ris = Mistero.Mistero1(x, y);
+            int ris2 = Mistero.mistero2(x, y);
+            int ris3 = Mistero.mistero2(x, y);
+            Console.Write("")
+            Console.WriteLine($" il mistero è {ris}, il mistero2 è {ris2}");
+            Console.ReadLine();
+           
+        } 
+    }
+}
