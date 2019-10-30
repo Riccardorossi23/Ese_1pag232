309fc98bd9b8d2657940643472b5d06970a33437
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-30 12:13:28 +0100

    EseMistero-pag232data30/10/19

--- ClassLibrary1pag232/Mistero.cs
+++ ClassLibrary1pag232/Mistero.cs
@@ -16,29 +16,96 @@ namespace ClassLibrary1pag232
             prec = Mistero1(x, y - 1) + 1;
             return prec;
         }
-        public static int mistero2(int x,int y)
+        public static int Mistero2(int x, int y)
         {
 
             if (y == 0)
                 return 0;
-            return (Mistero1(x, mistero2(x, y - 1)));
-            
+            return (Mistero1(x, Mistero2(x, y - 1)));
+
         }
-        public static int mistero3(int x,int y)
+        public static int Mistero3(int x, int y)
         {
             if (y == 0)
                 return 1;
-            return mistero2(x,mistero3(x,y-1)));
+            return Mistero2(x, Mistero3(x, y - 1));
         }
         public static int Mistero4(long n)
         {
-           
-            if 
+
+            Console.WriteLine(n % 10);
+            if (n / 10 != 0)
+            {
+                return Mistero4(n / 10);
+            }
+            throw new Exception("non andrà");
+
 
         }
-            
+        public static int Mistero5(int n1)
+        {
+            if (n1 == 0)
+            {
+                return 0;
+            }
+            else if (n1 == 1)
+            {
+                return 1;
+            }
+            return (Mistero5(n1 - 1) + Mistero5(n1 - 2));
+        }
+        public static int Mistero6(int x, int y)
+        {
+            if (x < 0)
+            {
+                return Mistero6(x + y, y);
+            }
+            else if (x < y)
+            {
+                return x;
+            }
+            else
+            {
+                return Mistero6(x - y, y);
+            }
+
+
+        }
+        public static int Mistero7(int x, int y)
+        {
+            int z = 1;
+            if (y < 0)
+            {
+                Console.WriteLine("Il valore è sbagliato");
+            }
+            else if (y > 0)
+            {
+                z = x * Mistero7(x, y - 1);
+            }
+            return z;
+
+
+        }
+        public static int Mistero8(int a, int b)
+        {
+            int z;
+            z = a % b;
+            if (z == 0)
+            {
+                return b;
+            }
+            else if (a == b && b == z)
+            {
+                return Mistero8(a , b);
+            }
+               
+        }
     }
 }
+
+              
+
+
 
    
 
--- Ese_1pag232/Program.cs
+++ Ese_1pag232/Program.cs
@@ -17,10 +17,22 @@ namespace Ese_1pag232
             Console.WriteLine("inserici il valore della y");
             int y = int.Parse(Console.ReadLine());
             int ris = Mistero.Mistero1(x, y);
-            int ris2 = Mistero.mistero2(x, y);
-            int ris3 = Mistero.mistero2(x, y);
-            Console.Write("")
-            Console.WriteLine($" il mistero è {ris}, il mistero2 è {ris2}");
+            int ris2 = Mistero.Mistero2(x, y);
+            int ris3 = Mistero.Mistero3(x, y);
+            int ris6 = Mistero.Mistero6(x, y);
+            int ris7 = Mistero.Mistero7(x, y);
+            Console.WriteLine("inserici il valore della n");
+            long n = long.Parse(Console.ReadLine());
+            long ris4 = Mistero.Mistero4(n);
+            Console.WriteLine("inserici il valore della a");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("inserici il valore della b");
+            int b = int.Parse(Console.ReadLine());
+            int ris8 = Mistero.Mistero8(a, b);
+            Console.WriteLine("inserici il valore della n");
+            int n1 = int.Parse(Console.ReadLine());
+            int ris5 = Mistero.Mistero5(n1);
+            Console.WriteLine($" il mistero è {ris}, il mistero2 è {ris2}, il mistero3 è {ris3}, il mistero4 è {ris4}, il mistero5 è {ris5}, il mistero6 è {ris6}, il mistero7 è {ris7}, il mistero8  è {ris8}");
             Console.ReadLine();
            
         } 
